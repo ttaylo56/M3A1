c9c3fcc5a8b61557e83d5a572ef819dec64cadd1
ttaylo56 <ttaylo56@asu.edu>
2023-02-11 21:15:18 -0800

    Second video finished

---
+++ utility.js
@@ -0,0 +1,5 @@
+export const generateRandomNumber = (minimum, maximum) => {
+    return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+}
+
+console.log(`from Utility: ${generateRandomNumber(10, 100)}`);
